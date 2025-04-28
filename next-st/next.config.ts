b323cc2301e014ba440861446b265bcddb641b6e
hupax <supanx11@gmail.com>
2025-04-28 17:57:40 +0800

    rm --cached next-st

--- next-st/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  swcMinify: true,
-}
-
-export default nextConfig
--- next-st/src/app/dashboard/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      Settings Content
-    </div>
-  )
-}
