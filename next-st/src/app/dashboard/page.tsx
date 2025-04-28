e17fea988330021c67dd2ee36518d9b1abb8217f
hupax <supanx11@gmail.com>
2025-04-28 17:55:20 +0800

    add next-st

---
+++ next-st/src/app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+// export default function page() {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+export default Page
